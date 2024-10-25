a56c7f5db40bc71db24b40717a0a00b2d5b09834
abhaysoni1 <soniabhay777@gmail.com>
2024-10-24 20:24:27 -0400

    Drawer

--- week7/App.tsx
+++ week7/App.tsx
@@ -1,9 +1,32 @@
+import "./gesture-handler";
 import { StatusBar } from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {NavigationContainer} from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
 
 function Home({navigation}) {
   return (
@@ -36,11 +59,15 @@ export default function App() {
   return (
 
       <NavigationContainer>
-        <Stack.Navigator>
+        {/* <Stack.Navigator>
           <Stack.Screen name="Home" component={Home}/>
           <Stack.Screen name="Settings" component={Settings}/>
           <Stack.Screen name="Hello" component={Hello}/>
-        </Stack.Navigator>
+        </Stack.Navigator> */}
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
       </NavigationContainer>
   );
 }
